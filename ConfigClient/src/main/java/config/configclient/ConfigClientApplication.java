a7bd62ae013e6deb76d6538a49c46963545efb81
Vikash Saini <vsai12@sapient.com>
2018-02-28 16:45:41 +0530

    commit

--- ConfigClient/src/main/java/config/configclient/ConfigClientApplication.java
+++ ConfigClient/src/main/java/config/configclient/ConfigClientApplication.java
@@ -1,12 +1,42 @@
 package config.configclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+}
+
+
+@RestController
+class ControllerClass{
+	
+	@Value("${message}")
+	private String messageValue;
+	
+	@RequestMapping("/messages")
+	String getMessage() {
+		return messageValue;
+	}
+}
+
+@RestController
+@RefreshScope
+class ControllerClass2{
+	
+	@Value("${holi}")
+	private String holiValue;
+	
+	@RequestMapping("/holi")
+	String getMessage() {
+		return holiValue;
 	}
 }
--- ConfigService/src/main/java/config/configservice/ConfigServiceApplication.java
+++ ConfigService/src/main/java/config/configservice/ConfigServiceApplication.java
@@ -3,6 +3,8 @@ package config.configservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableConfigServer
 @SpringBootApplication
@@ -10,5 +12,16 @@ public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServiceApplication.class, args);
+	}
+}
+
+
+
+@RestController
+class ControllerClass{
+	
+	@RequestMapping("/test")
+	String getMessage() {
+		return "I am up";
 	}
 }
